d563f4c631b3fa16851b04621fd7bf2e9dfb6aa0
Ivo Silva <silvaivosoares@gmail.com>
2022-05-19 11:45:06 +0100

    Completed all client routes

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -59,6 +59,74 @@ router.get('/clients', isAuthenticated, (req,res,next) => {
         })
 })
 
+//Get details of specific client
+router.get('/clients/:clientId', isAuthenticated, (req, res, next) => {
+    const {clientId} = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(clientId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Client.findById(clientId)
+        .populate('services')
+        .then(client => res.json(client))
+        .catch(error => {
+            console.log("Error getting the details of the client", error);
+            res.status(500).json({
+                message: "Error getting the details of the client",
+                error: error
+            });
+        })
+})
+
+//Update client data
+router.put('/clients/:clientId', isAuthenticated, (req,res,next) => {
+    const {clientId} = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(clientId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    const newDetails = {
+        name: req.body.name,
+        fiscalNumber: req.body.fiscalNumber,
+    }
+
+    Client.findByIdAndUpdate(clientId, newDetails, {new: true})
+    .then(updatedClient => res.json(updatedClient))
+    .catch(error => {
+        console.log("Error updating client details", error);
+        res.status(500).json({
+            message: "E",
+            error: error
+        });
+    })
+})
+
+// Delete client data
+router.delete('/clients/:clientId', isAuthenticated, (req,res,next) => {
+    const {clientId} = req.params;
+    const currentUserId = req.payload._id
+
+    Client.findByIdAndRemove(clientId)
+        .then(deletedClient => {
+            return Service.deleteMany({_id: {$in: deletedClient.services}});
+        })
+        .then(() => {
+            return User.findByIdAndUpdate(currentUserId, {$pull : {clients: clientId}})
+        })
+        .then(() => {
+            res.json({message: `Client and all associated services was removed successfully.`})
+        })
+        .catch(error => {
+            console.log("Error removing client.", error);
+            res.status(500).json({
+                message: "Error removing client.",
+                error: error
+            });
+        })
+})
 
 module.exports = router;
